26e221c606f65dfe0d5ccbd2f31c77ca59fcd248
Mads <madhulal@mozanta.com>
2020-07-17 19:25:19 +0530

    Added weeknd check

--- getdata.py
+++ getdata.py
@@ -5,6 +5,7 @@ from downloadbsebhavcopy import download_bse_bhavcopy
 from settings import LOG_FILE, DATA_FOLDER
 import os
 from datetime import datetime
+from holiday import istradingholiday
 
 today = datetime.today().date()
 logfile = open(LOG_FILE, 'r')
@@ -18,10 +19,12 @@ print(diff.days)
 
 for i in range(1, diff.days+1):
     requestdate = lastdate + relativedelta(days=i)
-    download_nse_bhavcopy(requestdate, DATA_FOLDER)
-    download_bse_bhavcopy(requestdate, DATA_FOLDER)
-    wr = requestdate.date()
-
+    if(istradingholiday(requestdate)):
+        print('Holiday')
+    else:
+        download_nse_bhavcopy(requestdate, DATA_FOLDER)
+        download_bse_bhavcopy(requestdate, DATA_FOLDER)
+        wr = requestdate.date()
 
 # writing the last downloaded date to LOG file
 if not isinstance(wr, str):
--- holiday.py
+++ holiday.py
@@ -1,18 +1,22 @@
 from datetime import datetime
 
-def is_weekend(date):
+
+def isweekend(date):
     weekno = date.weekday()
     if weekno >= 5:
-        return True
-    else: 
-        return False
-
-#TODO Check the trading holidays
-def is_trading_holiday(date):
-    if(is_weekend(date)):
         return True
     else:
         return False
 
+# TODO Check the trading holidays
+
+
+def istradingholiday(date):
+    if(isweekend(date)):
+        return True
+    else:
+        return False
+
+
 today = datetime.today().date()
-print(is_trading_holiday(today))
+print(istradingholiday(today))
