206006fd4a0dfca460c5dd9e75bae8d5f80f08f2
Mads <madhulal@mozanta.com>
2020-07-18 11:34:57 +0530

    Added more mongo operations

--- mongoutils.py
+++ mongoutils.py
@@ -6,12 +6,32 @@ client = MongoClient(MONGO_URI)
 db = client.marketdb
 
 
+def dbstatus():
+    print(db.collection_names())
+    status = db.command("dbstats")
+    print(status)
+
+
+def countrecors(collection):
+    n_records = db[collection].find().count()
+    print("There are {} cars".format(n_records))
+
+
 def insertrecord(collection, record):
     db[collection].insert(record)
 
 
 def insertrecords(collection, records):
     db[collection].insert_many(records)
+
+
+def updaterecord(collection, record):
+    db[collection].update_one(record, upsert=True)
+
+
+def getrecord(collection, idvalue):
+    record = db[collection].find_one({'_id': idvalue})
+    return record
 
 
 def printrecords(collection):
@@ -37,6 +57,6 @@ cars = [{'_id': 123, 'name': 'Audi', 'price': 52642},
         {'name': 'Hummer', 'price': 41400},
         {'name': 'Volkswagen', 'price': 21600}]
 # dropcollection("car")
-#insertrecord("car", cars)
+# insertrecord("car", cars)
 # listcollections()
 # printrecords("car")
